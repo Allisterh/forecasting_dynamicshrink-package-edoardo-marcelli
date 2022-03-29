3ed7107a9bae07649cf2800f89220f4b7e5a7409
edoardo-marcelli <edoardomarcelli2@gmail.com>
2022-03-29 21:39:13 +0200

    upload other functions

---
+++ DEMVS function.R
@@ -0,0 +1,174 @@
+# Dynamic EMVS
+#-------------
+
+DEMVS<-function(y,X,N,n0,d0,phi0,phi1,THETA,lambda0,lambda1,delta,new.X){
+  
+  require(matlib)
+  cat("Starting EMVS for dynamic Shrinkage in Dynamic Linear Model")
+  
+  # Preliminaries
+  #-----------------------------------------
+  
+  P<-dim(X)[2]
+  TIME<-length(y)
+  
+  ols = lm(y ~ X)
+  beta0 = ols$coefficients[-1]
+  beta0[beta0>10 | beta0<(-10)]=10
+  beta0[is.na(beta0)]=0
+  theta<-matrix(beta0,nrow=P,ncol=TIME+1)
+  n<-c()
+  d<-c()
+  n[1]<-n0
+  d[1]<-d0
+  v<-c()
+  precision<-c()
+  
+  bigX<-matrix(0,nrow=TIME,ncol=TIME*P)
+  j=1
+  for(t in 1:TIME){
+    bigX[t,j:(j+P-1)]<-X[t,]
+    j=j+P
+  }
+  
+  # Storing Objects
+  #------------------------------
+  
+  store_gamma<-array(NA,c(N,P,TIME+1))
+  store_theta <-array(NA,c(N,P,TIME+1))
+  store_v <-matrix(NA,N,TIME)
+  
+  # EM
+  #---------------------------------
+  
+  if(missing(new.X)){}else{
+    FF.1 = matrix(c(new.X),nrow=1)
+  } 
+  
+  ptm <- proc.time()
+  
+  for(i in 1:N){
+    
+    if(i %% 100==0) cat("\n", i, "loops...") 
+    if(i == N) cat("Done!\n") 
+    
+    # INDICATOR
+    
+    beta<-matrix(theta,ncol=1,byrow=T)
+    thetabeta<-matrix(theta[,2:(TIME+1)],ncol=1,byrow=T)
+    
+    thetaind = THETA*dnorm(beta[1:((TIME+1)*P-P),1],phi0,sqrt(lambda1/(1-phi1^2)))/(THETA*dnorm(beta[1:((TIME+1)*P-P),1],phi0,sqrt(lambda1/(1-phi1^2)))
+                                                                                    +(1-THETA)*dnorm(beta[1:((TIME+1)*P-P),1],0,sqrt(lambda0)))
+    
+    mu = phi0+phi1*(beta[1:((TIME+1)*P-P),1]-phi0)
+    
+    pstar = thetaind*dnorm(beta[(P+1):((TIME+1)*P),1],mu,sqrt(lambda1))/(thetaind*dnorm(beta[(P+1):((TIME+1)*P),1],mu,sqrt(lambda1))
+                                                                         +(1-thetaind)*dnorm(beta[(P+1):((TIME+1)*P),1],0,sqrt(lambda0)))
+    
+    pstar0 = thetaind[1:P]
+    
+    gamma = matrix(c(pstar0,pstar),ncol=P,byrow=T)
+    
+    # VOLATILITY
+    
+    res=y-bigX%*%thetabeta
+    
+    for(t in 2:(TIME+1)){
+      n[t]=delta*n[t-1]+1
+      d[t]=delta*d[t-1]+res[t-1]^2
+    }
+    
+    precision[TIME+1]=n[TIME+1]/d[TIME+1]
+    
+    v[TIME+1]=1/precision[TIME+1]
+    
+    for(s in (TIME+1):2){
+      
+      precision[s-1]=(1-delta)*n[s-1]/d[s-1]+delta*precision[s]
+      
+      v[s-1]=1/precision[s-1]
+    }
+    
+    precision = precision[2:(TIME+1)]
+    if(i<10){
+      precision = rep(4,TIME)
+    }else{}
+    
+    # COEFFICIENT
+    
+    for(t in 1:(TIME-1)){
+      
+      x = matrix(X[t,])
+      
+      D = diag(gamma[t+1,]/lambda1+(1-gamma[t+1,])/lambda0+
+                 (phi1^2)*gamma[t+2,]/lambda1)
+      
+      X1 = x%*%t(x)
+      
+      Sigma = precision[t]*X1+D
+      
+      invD = diag(1/diag(D))
+      
+      Den = 1 + precision[t]*t(x)%*%invD%*%x
+      
+      invDen = 1/Den[1,1]
+      
+      invSigma = invD - precision[t]*invD%*%(X1*invDen)%*%invD
+      
+      u=precision[t]*y[t]*x+phi1/lambda1*theta[,t]*gamma[t+1,]+(phi1/lambda1*theta[,t+2]*gamma[t+2,])
+      
+      theta[,t+1] = invSigma%*%u
+      
+    }
+    
+    D = diag(gamma[TIME+1,]/lambda1+(1-gamma[TIME+1,])/lambda0)
+    
+    x = matrix(X[TIME,])
+    
+    X1 = x%*%t(x)
+    
+    Sigma=precision[TIME]*x%*%t(x)+D
+    
+    u=precision[TIME]*y[TIME]*x+phi1/lambda1*theta[,TIME]*gamma[TIME+1,]
+    
+    invD = diag(1/diag(D))
+    
+    Den = 1 + precision[TIME]*t(x)%*%invD%*%x
+    
+    invDen = 1/Den[1,1]
+    
+    invSigma = invD - precision[TIME]*invD%*%(X1*invDen)%*%invD
+    
+    theta[,TIME+1]= invSigma%*%u
+    
+    Sigma = diag((1-phi1^2)*gamma[1,]/lambda1+(1-gamma[1,])/lambda0+(phi1^2)*gamma[2,]/lambda1)
+    
+    invSigma = diag(1/diag(Sigma))
+    
+    theta[,1] = phi1/lambda1*invSigma%*%theta[,2]*gamma[2,]
+    
+    # STORE SAMPLES
+    
+    store_gamma[i,,] = t(gamma) 
+    store_theta[i,,] = theta 
+    store_v[i,] = v[2:length(v)]
+    
+  }
+  
+  if(missing(new.X)){f = NULL }else{
+    
+    GG.1 = diag(phi1*gamma[dim(gamma)[1],],P)
+    state_vec = matrix(c(theta[,TIME+1]),ncol=1)
+    a = GG.1%*%state_vec
+    f = FF.1%*%a
+    
+  }
+  
+  cat("MCMC takes")
+  print(proc.time() - ptm)
+  
+  cat("For further details please consult Rockova & McAlinn 2021")
+  
+  return(list(beta=store_theta,fc.f = f,gamma=store_gamma,v=store_v))
+  
+}
---
+++ DSSTVP_PRECISIONSAMPLER_SVAR function.R
@@ -0,0 +1,308 @@
+# Dynamic SSVS (Precision Sampler) for TVP-SVAR models with SV
+#--------------------------------------------------------------
+
+DSSTVP_PRECISIONSAMPLER_SVAR = function(X,constant,lags,N,burn,THETA,lambda1,lambda0,phi1,phi0,gamma0,v0,start_params,start_latent){
+  
+  # Packages
+  
+  require(Matrix)
+  require(stochvol)
+  require(pracma)
+  require(Rcpp)
+  require(MASS)
+  require(matlib)
+  require(mvtnorm)
+  require(matrixStats)
+  cppFunction(plugins=c("cpp11"), "NumericVector cpprbinom2(int n, double size, NumericVector prob) { 
+    NumericVector v = no_init(n);
+    std::transform( prob.begin(), prob.end(), v.begin(), [=](double p){ return R::rbinom(size, p); }); 
+    return(v);}")
+  require(bvarsv)
+  list <- unclass(lsf.str(envir = asNamespace("bvarsv"), all = T))[]
+  for(i in list){
+    assign(paste0(i),eval(parse(text = paste('bvarsv:::', i, sep=''))))
+  }
+  
+  
+  # Preliminaries
+  
+  TIME = dim(X)[1]
+  n = dim(X)[2]
+  h=lags
+  m = n*(n-1)/2
+  PA = sum(1:(n-1))
+  P = n^2*h+PA
+  step_ahead=1
+  np=sum(1:n)
+  
+  XX<-X[(h+1):TIME,]
+  Y = matrix(t(XX),ncol=1)*10
+  bigY<-matrix(NA,nrow=TIME-h,ncol=n*h)
+  j=1
+  for(i in h:1){
+    bigY[,j:(j+n-1)]<-X[i:(TIME-h+i-1),]
+    j=j+n  
+  }
+  
+  bigY.new<-matrix(NA,nrow=TIME-h+1,ncol=n*h)
+  j=1
+  for(i in h:1){
+    bigY.new[,j:(j+n-1)]<-X[i:(TIME-h+i),]
+    j=j+n  
+  }
+  
+  TIME = dim(bigY)[1]
+  
+  if(constant==TRUE){
+    bigY = cbind(matrix(1,nrow=dim(bigY)[1],ncol=1),bigY)
+    bigY.new = cbind(matrix(1,nrow=dim(bigY.new)[1],ncol=1),bigY.new)
+    P = P + n
+  }else{}
+  
+  Fill_bigX<-list()
+  for(i in 1:dim(bigY)[1]){
+    block_bigX = t(kronecker(diag(1,n),bigY[i,]))
+    
+    block_bigX2 = matrix(0, nrow=n, ncol=sum(1:(n-1)))
+    k=0
+    j=1
+    for(s in 1:(n-1)){
+      block_bigX2[2+k,j:(j+length(seq(1:s))-1)] = -XX[i,1:(1+k)]
+      k = k+1
+      j = j+length(seq(1:s))
+    }
+    
+    Fill_bigX[[i]] = cbind(block_bigX,block_bigX2)
+  }
+  bigX = .bdiag(Fill_bigX)
+  
+  FF.new = Fill_bigX[[dim(bigY)[1]]]
+  
+  # Stocvol Package options
+  
+  if(missing(start_params)){
+    params <- list(mu = -2, phi = 0.9, sigma = 0.1,
+                   nu = Inf, rho = 0, beta = NA,
+                   latent0 = -2)
+  }else{
+    params <-start_params
+  }
+  
+  if(missing(start_latent)){
+    start_latent = rep(-2, TIME)
+  }else{
+    start_latent = start_latent
+  }
+  
+  #Store
+  
+  store_gammino = matrix(NA,N,TIME*P)
+  store_gammino0 = matrix(NA,N,P)
+  store_Sigtheta = matrix(NA,N,TIME*P)
+  store_Sigtheta0 = matrix(N,1)
+  store_theta = matrix(NA,N,TIME*P)
+  store_theta0 = matrix(NA,N,P)
+  store_h = matrix(NA,N,TIME*n)
+  store_fc.m<-array(NA,c(n,step_ahead,N))
+  store_fc.y<-array(NA,c(n,step_ahead,N))
+  store_fc.v<-array(NA,c(np,step_ahead,N))
+  
+  # Initialize
+  
+  h = matrix(log(v0),TIME*n,1)
+  h1 = matrix(h,ncol=n,byrow=T)
+  h.new = matrix(NA,nrow=1,ncol=n)
+  theta0 = matrix(0,P,1)
+  gammino = gamma0*matrix(1,TIME*P,1)
+  gammino0 = gamma0*matrix(1,P,1)
+  
+  Htheta = Matrix(nrow = TIME*P, ncol = TIME*P, data = 0, sparse = TRUE)
+  diag(Htheta)=1
+  Htheta = Htheta - sparseMatrix(i=c((P+1):(TIME*P)),j=c(1:((TIME-1)*P)),
+                                 x=c(phi1*gammino[(P+1):length(gammino)]),dims=c(TIME*P,TIME*P))
+  
+  Sigtheta = sparseMatrix(i=c(1:(TIME*P)),j=c(1:(TIME*P)),
+                          x=c(gammino*lambda1+(1-gammino)*lambda0),dims=c(TIME*P,TIME*P))
+  
+  Sigtheta0 = gammino0*lambda1+(1-gammino0)*lambda0
+  
+  invSig = Diagonal(TIME*n)
+  invS = Diagonal(TIME*P)
+  a0 = Matrix(nrow=TIME*P,ncol=1,data=0,sparse=T)
+  Ktheta0 = Diagonal(P)
+  I_tp=Diagonal(TIME*P)
+  
+  # START GIBBS SAPLING
+  
+  ptm <- proc.time()
+  
+  for(it in 1:(N+burn)){
+    
+    if(it %% 100==0) cat("\n", it, "loops...") 
+    if(it == N+burn) cat("Done!\n") 
+    
+    # Sample states 1:T
+    
+    # diag(invSig)=c(exp(-h))
+    diag(invSig)=c(exp(h))
+    
+    vec = 1/diag(Sigtheta)
+    diag(invS) = vec
+    
+    XinvSig = t(bigX)%*%invSig
+    
+    HinvSH = t(Htheta)%*%invS%*%Htheta
+    
+    a0[1:P] =  phi1*gammino[1:P,1]*theta0
+    
+    alptheta = solve(Htheta,a0)
+    
+    Ktheta = HinvSH + XinvSig%*%bigX
+    
+    dtheta = XinvSig%*%Y + HinvSH%*%alptheta
+    
+    thetahat = solve(Ktheta,dtheta)
+    
+    theta = thetahat + solve(t(chol(Ktheta)),rnorm(TIME*P))
+    
+    # Sample state 0
+    
+    vec = 1/diag(Sigtheta0)
+    diag(Ktheta0) = vec
+    
+    a0.0 = theta[1:P,]/Sigtheta0
+    theta0hat = solve(Ktheta0,a0.0)
+    
+    theta0 = theta0hat + solve(t(chol(Ktheta0)),rnorm(P))
+    
+    # Sample indicators
+    
+    
+    thetaind = THETA*dnorm(theta[(P+1-P):(TIME*P-P),1],phi0,sqrt(lambda1/(1-phi1^2)))/(THETA*dnorm(theta[(P+1-P):(TIME*P-P),1],phi0,sqrt(lambda1/(1-phi1^2)))+
+                                                                                         (1-THETA)*dnorm(theta[(P+1-P):(TIME*P-P),1],0,sqrt(lambda0)))
+    
+    mu = phi0+phi1*(theta[(P+1-P):(TIME*P-P),1]-phi0)
+    
+    
+    pstar = thetaind*dnorm(theta[(P+1):(TIME*P),1],mu,sqrt(lambda1))/(thetaind*dnorm(theta[(P+1):(TIME*P),1],mu,sqrt(lambda1))+
+                                                                        (1-thetaind)*dnorm(theta[(P+1):(TIME*P),1],0,sqrt(lambda0)))
+    
+    pstar[is.na(pstar)]=0
+    gammino[(P+1):(TIME*P),1] = cpprbinom2(TIME*P-P,1,pstar)
+    
+    
+    thetaind = THETA*dnorm(theta0[1:P,1],phi0,sqrt(lambda1/(1-phi1^2)))/(THETA*dnorm(theta0[1:P,1],phi0,sqrt(lambda1/(1-phi1^2)))
+                                                                         +(1-THETA)*dnorm(theta0[1:P,1],0,sqrt(lambda0)))
+    
+    mu = phi0+phi1*(theta0[1:P,1]-phi0)
+    
+    pstar = thetaind*dnorm(theta[1:P,1],mu,sqrt(lambda1))/
+      (thetaind*dnorm(theta[1:P,1],mu,sqrt(lambda1))+(1-thetaind)*dnorm(theta[1:P,1],0,sqrt(lambda0)))
+    
+    pstar[is.na(pstar)]=0
+    
+    gammino[1:P,1] = cpprbinom2(P,1,pstar)
+    
+    thetaind[is.na(thetaind)]=0
+    
+    gammino0[1:P,1] = cpprbinom2(P,1,thetaind)
+    
+    # Compute state variance matrix
+    
+    diag(Sigtheta) = c(gammino*lambda1+(1-gammino)*lambda0)
+    
+    Sigtheta0 = gammino0*lambda1+(1-gammino0)*lambda0
+    
+    # Compute H theta
+    
+    Htheta = I_tp - sparseMatrix(i=c((P+1):(TIME*P)),j=c(1:((TIME-1)*P)),
+                                 x=c(phi1*gammino[(P+1):length(gammino)]),dims=c(TIME*P,TIME*P))
+    
+    # Stocvol
+    
+    res = Y-bigX%*%theta
+    res1 = matrix(res,ncol=n,byrow=T)
+    
+    if(it<100){
+      h=rep(-10,TIME*n)
+      h.new = matrix(-10,nrow=1,ncol=n)
+    }else{
+      
+      for(i in 1:n){
+        res_fast = svsample_fast_cpp(res1[,i],
+                                     startpara = params, startlatent = start_latent)
+        
+        h1[,i] = res_fast$latent
+        sigzeta = res_fast$para[1,3]
+        alpha1h = res_fast$para[1,2]
+        alpha0h = res_fast$para[1,1]
+        h.new[,i] = alpha0h+alpha1h*h1[TIME,i]+rnorm(1,0,sigzeta)
+      }
+      h = matrix(t(h1),ncol=1)
+    }
+    
+    if(it > burn){
+      store_theta[it-burn,] = t(theta)[,1:(TIME*P)]
+      store_h[it-burn,] = t(h)
+      store_theta0[it-burn,] = t(theta0)[1:P]
+      store_gammino[it-burn,] = t(gammino)
+      store_gammino0[it-burn,] = t(gammino0)
+      
+      # FORECAST
+      
+      thetaind.new = THETA*dnorm(theta[(((TIME)*P)-P+1):((TIME)*P),1],phi0,sqrt(lambda1/(1-phi1^2)))/(THETA*dnorm(theta[(((TIME)*P)-P+1):((TIME)*P),1],phi0,sqrt(lambda1/(1-phi1^2)))
+                                                                                                      +(1-THETA)*dnorm(theta[(((TIME)*P)-P+1):((TIME)*P),1],0,sqrt(lambda0)))
+      
+      gammino.new = cpprbinom2(P,1,thetaind.new)
+      
+      WW.new = diag(gammino.new*lambda1+(1-gammino.new)*lambda0)
+      V.new = diag(c(exp(h.new)))
+      
+      m.new = thetahat[(TIME*P-P+1):(TIME*P),1]
+      GG.new = diag(phi1*gammino.new)
+      a.new = GG.new%*%m.new
+      f.new = FF.new%*%a.new
+      
+      C.new = solve(Ktheta[(TIME*P-P+1):(TIME*P),(TIME*P-P+1):(TIME*P)])
+      R.new = (GG.new)%*%C.new%*%t(GG.new)+WW.new
+      Q.new = FF.new%*%R.new%*%t(FF.new)+V.new
+      Q.new.mat = as.matrix(Q.new)
+      
+      store_fc.m[,,it-burn]<-f.new
+      store_fc.v[,,it-burn]<-vechC(Q.new.mat)
+      store_fc.y[,,it-burn]<-t(rmvnorm(1,mean=f.new,sigma= Q.new.mat))
+      
+      
+    }else{}
+    
+  }
+  
+  cat("MCMC takes:")
+  print(proc.time() - ptm)
+  
+  est.theta = colMedians(store_theta)
+  est.h = colMedians(store_h)
+  est.theta0 = colMedians(store_theta0)
+  est.gammino = colMedians(store_gammino)
+  est.gammino0 = colMedians(store_gammino0)
+  est.fcy = rowMedians(store_fc.y[,1,])
+  est.fcm = rowMedians(store_fc.m[,1,])
+  est.fcv = rowMedians(store_fc.v[,1,])
+  
+  theta = matrix(est.theta,nrow=TIME,ncol=P,byrow=T)
+  h = matrix(est.h,nrow=TIME,ncol=n,byrow=T)
+  theta0 = matrix(est.theta0,nrow=TIME,ncol=P,byrow=T)
+  gammino = matrix(est.gammino,nrow=TIME,ncol=P,byrow=T)
+  gammino0 = matrix(est.gammino0,nrow=TIME,ncol=P,byrow=T)
+  
+  
+  return(list(beta = theta/10, h=h,beta0 = theta0/10, ind=gammino, ind0=gammino0,
+              beta.sample=store_theta, h.sample=store_h
+              ,beta0.sample=store_theta0,
+              gammino.sample=store_gammino,gammino0.sample=store_gammino,
+              bigX=bigX,Y=Y,
+              THETA=THETA,lambda0=lambda0,lambda1=lambda1,TIME=TIME,X=X,phi0=phi0,
+              phi1=phi1,
+              fc.m = est.fcm, fc.y = est.fcy, fc.v = est.fcv
+  ))
+} 
